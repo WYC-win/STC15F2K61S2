ba6557de7758b423e2ad3580f67091c0d70dfb58
WYC-win <3540808840@qq.com>
2026-03-04 20:42:58 +0800

    简单调试

--- 16届蓝桥杯真题/main.c
+++ 16届蓝桥杯真题/main.c
@@ -11,7 +11,8 @@
 #include "superwave.h"
 void environment();
 void sport();
-int tem,i,distance;
+void Parameter();
+int tem,i,distance,counter=0,counter1=0,tem_para=30,dis_para=30;
 unsigned char light_intensity_digtal,light_intensity;
 void main()
 {
@@ -22,8 +23,10 @@ void main()
 	delay(750);
 	while(1)
 	{
-//		environment();
-		sport();
+		if(counter==0)
+		{
+			Parameter();
+		}
 	}
 }
 
@@ -32,8 +35,8 @@ void environment()
 	
 	DS18B20_convertT();
 	light_intensity_digtal=AD_read(0x01);
-	if(light_intensity_digtal>=151)light_intensity=1;
-	else if(light_intensity_digtal>=102&&light_intensity_digtal<151)light_intensity=2;
+	if(light_intensity_digtal>=153)light_intensity=1;
+	else if(light_intensity_digtal>=102&&light_intensity_digtal<153)light_intensity=2;
 	else if(light_intensity_digtal>=25.5&&light_intensity_digtal<102)light_intensity=3;
 	else if(light_intensity_digtal<151)light_intensity=4;
 	for(i=0;i<200;i++)
@@ -50,9 +53,57 @@ void environment()
 void sport()
 {
 	int dis1,dis2;
-	
+	digital_tube(1,0xC7);//L
 	digital_tubefixed(6,distance/100);
 	digital_tubefixed(7,distance%100/10);
 	digital_tubefixed(8,distance%100%10);
 	distance=superwave_getdistance();
+}
+
+void Parameter()//参数界面
+{
+	if(counter1==0)
+	{
+		digital_tube(1,0x8C);//P
+		digital_tubefixed(2,12);
+		digital_tubefixed(7,tem_para/10);
+		digital_tubefixed(8,tem_para%10);
+		if(keynumber()==8)
+		{
+			tem_para++;
+		}
+		else if(keynumber()==9)
+		{
+			tem_para--;
+		}
+		else if(keynumber()==8 && keynumber()==9)
+		{
+			tem_para=0;
+		}
+	}
+	if(counter1==1)
+	{
+		digital_tube(1,0x8C);//P
+		digital_tube(2,0xC7);//L
+		digital_tubefixed(7,dis_para/10);
+		digital_tubefixed(8,dis_para%10);
+		if(keynumber()==8)
+		{
+			dis_para++;
+		}
+		else if(keynumber()==9)
+		{
+			dis_para--;
+		}
+		else if(keynumber()==8 && keynumber()==9)
+		{
+			dis_para=0;
+		}
+	}
+	if(keynumber()==5)
+	{
+		counter1++;
+		if(counter1==2)counter1=0;
+	}
+	
 }
