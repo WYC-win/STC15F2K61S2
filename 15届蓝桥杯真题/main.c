364891e613c9ddbc3ce18891c1b8f133c2af94d4
WYC-win <3540808840@qq.com>
2026-03-27 10:50:13 +0800

    完成界面显示

--- 15届蓝桥杯真题/main.c
+++ 15届蓝桥杯真题/main.c
@@ -15,7 +15,7 @@
 #define page_para_p2 1
 
 #define page_back_freq 0
-#define page_back_time 0
+#define page_back_time 1
 
 
 
@@ -35,7 +35,7 @@ int freq_real=0;
 unsigned int freq_ex=2000;
 int freq_adjust=0;
 
-unsigned char freq_max=0;
+unsigned int freq_max=0;
 
 
 void page_freq_display()
@@ -83,14 +83,91 @@ void page_freq_display()
 	
 }
 
+void page_para_display()
+{
+	digital_tube(1,0x8C);//P
+	if(page_para_count==0)
+	{
+		digitaltube_fixed(2,1);
+		digitaltube_fixed(5,freq_ex/1000);
+		digitaltube_fixed(6,freq_ex%1000/100);
+		digitaltube_fixed(7,freq_ex%100/10);
+		digitaltube_fixed(8,freq_ex%10);
+	}
+	else
+	{
+		digitaltube_fixed(2,2);
+		if(freq_adjust>0)
+		{
+				digitaltube_fixed(6,freq_adjust/100);
+				digitaltube_fixed(7,freq_adjust%100/10);
+				digitaltube_fixed(8,freq_adjust%10);
+			
+		}
+		else if(freq_adjust<0)
+		{
+			int tem_dat;
+			tem_dat=0-freq_adjust;
+			digital_tube(5,0xBF);//-
+			digitaltube_fixed(6,tem_dat/100);
+			digitaltube_fixed(7,tem_dat%100/10);
+			digitaltube_fixed(8,tem_dat%10);
+		}
+		else
+		{
+			digitaltube_fixed(8,freq_adjust);
+		}
+
+	}
+}
 
 
-
+void page_back_display()
+{
+	digital_tube(1,0x89);//H
+	if(page_back_count==0)
+	{
+		digitaltube_fixed(2,15);
+		if(freq_max/10000!=0)
+		{
+			digitaltube_fixed(4,freq_max/10000);
+			digitaltube_fixed(5,freq_max%10000/1000);
+			digitaltube_fixed(6,freq_max%1000/100);
+			digitaltube_fixed(7,freq_max%100/10);
+			digitaltube_fixed(8,freq_max%10);
+		}
+		else if(freq_max/1000!=0)
+		{
+			digitaltube_fixed(5,freq_max/1000);
+			digitaltube_fixed(6,freq_max%1000/100);
+			digitaltube_fixed(7,freq_max%100/10);
+			digitaltube_fixed(8,freq_max%10);
+		}
+		else if(freq_max/100!=0)
+		{
+			digitaltube_fixed(6,freq_max/100);
+			digitaltube_fixed(7,freq_max%100/10);
+			digitaltube_fixed(8,freq_max%10);
+		}
+		else if(freq_max/100!=0)
+		{
+			digitaltube_fixed(7,freq_max/10);
+			digitaltube_fixed(8,freq_max%10);
+		}
+		else if(freq_max/10!=0)
+		{
+			digitaltube_fixed(8,freq_max/10);
+		}
+	}
+	else
+	{
+		digitaltube_fixed(2,10);
+	}
+}
 void main()
 {
 	init();
 	while(1)
 	{
-		page_freq_display();
 	}
 }
