89e5fabd6e90ffbbac52bf6ab5e6f3a764f73c37
WYC-win <3540808840@qq.com>
2026-04-10 20:25:15 +0800

    调试ds18b20

--- 模块练习/ds18b20.c
+++ 模块练习/ds18b20.c
@@ -8,15 +8,16 @@ void  Convert_T()
 	Write_DS18B20(0x44);
 }
 
- int get_temperature()
+ float get_temperature()
 {
-	unsigned int temp;
+	 int temp;
 	unsigned char MSB,LSB;
 	init_ds18b20();
 	Write_DS18B20(0xCC);
 	Write_DS18B20(0xBE);
 	LSB=Read_DS18B20();
 	MSB=Read_DS18B20();
-	temp=(((int)MSB<<8)|LSB)/16;
-	return temp;
+	temp=(((int)MSB<<8)|LSB);
+	if((temp&0x8000)==0x0000)return temp*0.0625;
+	else return (temp-65536)*0.0625;
 }
